03acda8bfa58031ae5ad130b5db34911ae180e25
UbuntuJohn <John@new-host-4.home>
2015-06-13 01:09:36 -0400

    Comments for login page

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -2,21 +2,26 @@
 session_start();
 
 class Login extends CI_Controller {
-	
+	//process method accessing database functions
 	public function process() {
 		$this->load->database();
 		
-		
+		//check to see if email and password is set
 		if(isset($_POST['email']) && isset($_POST['password'])) {
+			//checking to see if values are NOT empty
 			if(!empty($_POST['email']) && !empty($_POST['password'])) {
+				//setting up variables to values
 				$email = $this->input->post('email');
 				$pass = $this->input->post('password');
+				//secure salt
 				$salt = "fullsail";
+				//password hashing
 				$hashed = crypt($pass, $salt);
 
-				//Do our database stuff here!
+				//Do our database stuff here! *Bad Practice, will change later*
 				$query = $this->db->query("select employeeId, firstName, lastName, startDate, manager from employee where email='{$email}' and password='{$hashed}' limit 1");
 
+				//check to see if account exists
 				if($query->num_rows() > 0) {
 
 					
@@ -34,8 +39,9 @@ class Login extends CI_Controller {
 						}
 					
 					} */
-
+					//setting up variable for each view
 					$row = $query->row();
+					//create a session for each value shown
 					$_SESSION['firstName'] = $row->firstName;
 					$_SESSION['employeeId'] = $row->employeeId;
 
@@ -43,11 +49,12 @@ class Login extends CI_Controller {
 					$array = [
 						
 					];
-
+					//send user to dashboard
 					header("Location: ../dashboard/");
 					
 
 				} else {
+					//show login process form for error handling
 					$this->load->view('loginProcess');
 				}
 
