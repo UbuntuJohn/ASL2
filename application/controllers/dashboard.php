22b6320de7c18e9ef25649ff7bc834801fd968c0
UbuntuJohn <John@new-host-4.home>
2015-06-13 01:05:14 -0400

    Comments added to Dashboard

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -23,23 +23,29 @@ class Dashboard extends CI_Controller {
 	 */
 	public function index()
 	{
-
+		//check to see if session isn't set
 		if(!$_SESSION) {
+			//if not, send user to login page (where it is initallized after login)
 			header("Location: login");
 		}
+		//load the data helper
 		$this->load->helper('date');
 		//$this->load->view('dashboard'); //this was causing a bug with showing template TWICE
+		//load the session library
 		$this->load->library('session');
+		//load the template parsing library
 		$this->load->library('parser');
+		//load the database functions
 		$this->load->database();
-		
+		//changet he default timezone to Eastern Time
 		date_default_timezone_set('America/New_York');
 
-
+		//SQL even though it's bad practice, I'll be changing in a future update
 		$query = $this->db->query("select * from team join employee on employee.employeeId = team.managerId where managerId='{$_SESSION['employeeId']}'");
 
+		//check if number of rows is greater than 0 (if team exists)
 		if($query->num_rows() > 0) {
-
+			//an array to hold template parsing values
 			$data = array(
 				'user' => $_SESSION['firstName'],
 				'employeeId' => $_SESSION['employeeId'],
@@ -50,7 +56,7 @@ class Dashboard extends CI_Controller {
 			);
 				
 		} else {
-			
+			//same as above, but if team doesn't exist
 			$data = array(
 				'user' => $_SESSION['firstName'],
 				'employeeId' => $_SESSION['employeeId'],
@@ -59,26 +65,28 @@ class Dashboard extends CI_Controller {
 
 			
 		}
-
+		//parse the templates using the $data arrays above
 		$this->parser->parse('dashboard', $data);
 
 			
 			
 	}
-
+	//login method for loading form, html processing, viewing login template, sessions and URL
 	public function login() {
 		$this->load->helper('form');
 		$this->load->helper('html');
 		$this->load->view('login');
 		$this->load->library('session');
 		$this->load->helper('url');
-
+		//check if session exists and send user to dashboard
 		if($_SESSION) {
 			$base_url = base_url()."index.php/dashboard/";
 			header("Location: {$base_url}");
 		}
 	}
 
+	//logout method showing the logout template and allowing for javascript to run
+	//also has session unsetting and destroying
 	public function logout() {
 		$this->load->view('logout');
 		$this->load->library('javascript');
@@ -88,6 +96,7 @@ class Dashboard extends CI_Controller {
 		session_destroy();
 	}
 
+	//Future methods below...
 	public function profile() {
 
 	}
@@ -106,5 +115,5 @@ class Dashboard extends CI_Controller {
 
 }
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
+/* End of file dashboard.php */
+/* Location: ./application/controllers/dashboard.php */
