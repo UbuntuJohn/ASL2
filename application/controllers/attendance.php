11a43b4ca485da092863873f14e592f26e8009df
UbuntuJohn <John@new-host-4.home>
2015-06-24 00:39:31 -0400

    Fixed attendance bug

--- application/controllers/attendance.php
+++ application/controllers/attendance.php
@@ -11,9 +11,9 @@ class Attendance extends CI_Controller {
 		$employeeId = $_POST['employeeId'];
 
 		if($attCode == "Late") {
-			$attCode = 1;
-		} else {
 			$attCode = 2;
+		} else {
+			$attCode = 1;
 		}
 
 		$sql = "insert into attendance (date, attendanceCode, description, employeeId) values ('$date', '$attCode', '$description', '$employeeId')";
